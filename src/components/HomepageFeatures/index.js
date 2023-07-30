9618ccc942f0d74cf2ca50be2ab5b458a762534c
nourilee <nourilee@launchpadrecruits.com>
2023-07-30 18:36:39 +0800

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,20 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Foundational Concepts and Methodologies',
+    Svg: require('@site/static/img/learning.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Master the core principles of Quality Assurance (QA) in software testing. Understand the software development lifecycle, test types, design strategies, and defect management. Build a solid testing foundation to approach challenges with confidence!
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Technical and Practical Skills',
+    Svg: require('@site/static/img/it-girl.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Unlock your potential with hands-on testing expertise! Develop manual testing finesse, embrace automation frameworks, dive into API testing, and explore performance evaluation. Acquire the skills to excel in real-world QA scenarios and thrive in your QA role!
       </>
     ),
   },
@@ -37,7 +25,7 @@ const FeatureList = [
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--6')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
