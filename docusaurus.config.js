a4d9287b993bc442a21f032f125dfe204dbcf4a7
nourilee <nourilee@launchpadrecruits.com>
2023-08-10 19:42:39 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nourilee', // Usually your GitHub org/user name.
   projectName: 'qa-handbook', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
